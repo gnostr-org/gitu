b6252c4b2925f2fdc8c762a2c8414f9cb4b3b590
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-26 16:49:13 -0400

    ci/install-dependencies.sh
    brew install --with-clang llvm

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -34,8 +34,8 @@ macos-*)
 	export HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1
 
 	# Uncomment this if you want to run perf tests:
-	brew install gnu-time
-	brew install          clang
+	brew install gnu-time && \
+	brew install --with-clang llvm
 
 	test -z "$BREW_INSTALL_PACKAGES" ||
 	brew install $BREW_INSTALL_PACKAGES
